eef09de103bf83c06153edaf4d193954bae71f5c
John Crosby <john.crosby@mlb.com>
2021-04-03 11:42:15 -0400

    feat: updated index

--- pages/index.js
+++ pages/index.js
@@ -1,55 +1,34 @@
 import Head from 'next/head';
-import Layout, { siteTitle } from '../components/layout';
-import utilStyles from '../styles/utils.module.css';
-import { getSortedPostsData } from '../lib/posts';
 import Link from 'next/link';
+import useSWR from 'swr';
 import Date from '../components/date';
-
-export async function getStaticProps() {
-    const allPostsData = getSortedPostsData();
-    return {
-        props: {
-            allPostsData,
-        },
-    };
-}
-
-export default function Home({ allPostsData }) {
+import Game from '../components/games/game';
+import styles from '../components/layout.module.css';
+import { getTodaysGames } from '../lib/games';
+import utilStyles from '../styles/utils.module.css';
+export default function Home() {
+    const { data: games, error } = useSWR('api/games/today', getTodaysGames);
     return (
-        <Layout home>
+        <div className={styles.container}>
             <Head>
-                <title>{siteTitle}</title>
+                <title>Games</title>
             </Head>
-            <section className={utilStyles.headingMd}>
-                <p>
-                    Hi, I'm John, a former professional cook turned keyboard
-                    jockey.
-                </p>
-                <p>
-                    (This is a sample website - you’ll be building a site like
-                    this on{' '}
-                    <a href='https://nextjs.org/learn'>our Next.js tutorial</a>
-                    .)
-                </p>
-            </section>
             <section
                 className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}
             >
-                <h2 className={utilStyles.headingLg}>Blog</h2>
-                <ul className={utilStyles.list}>
-                    {allPostsData.map(({ id, date, title }) => (
-                        <li className={utilStyles.listItem} key={id}>
-                            <Link href={`/posts/${id}`}>
-                                <a>{title}</a>
-                            </Link>
-                            <br />
-                            <small className={utilStyles.lightText}>
-                                <Date dateString={date} />
-                            </small>
-                        </li>
-                    ))}
-                </ul>
+                <ol className={utilStyles.list}>
+                    {games?.gameData?.results?.map((game) => {
+                        return (
+                            <li
+                                className={utilStyles.listItem}
+                                key={game.gamePk}
+                            >
+                                <Game {...game} />
+                            </li>
+                        );
+                    })}
+                </ol>
             </section>
-        </Layout>
+        </div>
     );
 }
