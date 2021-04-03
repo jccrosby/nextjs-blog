9e83ca54953b44cef15e8f04c270e098f4fd671c
John Crosby <john.crosby@mlb.com>
2021-04-03 11:41:13 -0400

    feat: game componnet or display

--- components/date.js
+++ components/date.js
@@ -2,5 +2,7 @@ import { parseISO, format } from 'date-fns';
 
 export default function Date({ dateString }) {
     const date = parseISO(dateString);
-    return <time dateTime={dateString}>{format(date, 'LLLL d, yyyy')}</time>;
+    return (
+        <time dateTime={dateString}>{format(date, 'M/d/yyyy h:mm aaa')}</time>
+    );
 }
--- pages/today.js
+++ pages/today.js
@@ -2,14 +2,14 @@ import Head from 'next/head';
 import Link from 'next/link';
 import useSWR from 'swr';
 import Date from '../components/date';
-import Layout from '../components/layout';
+import Game from '../components/games/game';
+import styles from '../components/layout.module.css';
 import { getTodaysGames } from '../lib/games';
 import utilStyles from '../styles/utils.module.css';
-
 export default function Home() {
     const { data: games, error } = useSWR('api/games/today', getTodaysGames);
     return (
-        <Layout home>
+        <div className={styles.container}>
             <Head>
                 <title>Games</title>
             </Head>
@@ -17,19 +17,18 @@ export default function Home() {
                 className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}
             >
                 <ol className={utilStyles.list}>
-                    {games?.gameData?.results?.map(({ gamePk, gameData }) => (
-                        <li className={utilStyles.listItem} key={gamePk}>
-                            <Link href={`https://www.mlb.com/tv/g${gamePk}`}>
-                                <a>{gamePk}</a>
-                            </Link>
-                            <br />
-                            <small className={utilStyles.lightText}>
-                                <Date dateString={gameData.gameDate} />
-                            </small>
-                        </li>
-                    ))}
+                    {games?.gameData?.results?.map((game) => {
+                        return (
+                            <li
+                                className={utilStyles.listItem}
+                                key={game.gamePk}
+                            >
+                                <Game {...game} />
+                            </li>
+                        );
+                    })}
                 </ol>
             </section>
-        </Layout>
+        </div>
     );
 }
