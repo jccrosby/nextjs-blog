76d57769fb8b8aeafcbf29ca8f079df5e972cee1
John Crosby <john.crosby@mlb.com>
2021-04-03 10:06:07 -0400

    fix: await/async

--- pages/api/games/today.js
+++ pages/api/games/today.js
@@ -1,7 +1,11 @@
-export default function handler(req, res) {
-    const gameData = await fetch('https://mastapi.mobile.mlbinfra.com/api/epg/v1/search?hydrate=xrefId,linescore(matchup,runners),team(venue(timezone),league),probablePitcher,person,decisions,flags,stats')
+export default async function handler(req, res) {
+    const gameData = await fetch(
+        'https://mastapi.mobile.mlbinfra.com/api/epg/v1/search?hydrate=xrefId,linescore(matchup,runners),team(venue(timezone),league),probablePitcher,person,decisions,flags,stats',
+    )
+        .then((response) => response.json())
+        .catch((error) => JSON.parse(JSON.stringify(error)));
     res.status(200).json({
         dateTime: new Date(),
-        gameData
+        gameData,
     });
 }
